0d9338c384709bbe9e46c00306bbad7b6d804ff1
Kubeleusz <kubeleusz@gmail.com>
2020-01-06 16:08:52 +0100

    Panel nawigacji + routing

--- simulator-ng-app/src/app/app.module.ts
+++ simulator-ng-app/src/app/app.module.ts
@@ -3,14 +3,45 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HelpComponent } from './help/help.component';
+import { MainComponent } from './main/main.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+
+const appRoutes :Routes = [
+  {
+    path:'main',
+    component:MainComponent
+  },
+  {
+    path:'help',
+    component:HelpComponent
+  },
+  {
+    path:'',
+    component:MainComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HelpComponent,
+    MainComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true}) //shows Router Events
   ],
   providers: [],
   bootstrap: [AppComponent]
